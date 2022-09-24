e6557212f16b9d69a6a121eaa788ea01a6b8c44c
zicoprmd <zicoprmd@gmail.com>
2022-09-24 13:59:17 +0700

    added img banner zema

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
-import banner from '../assets/banner/IMG_9741.JPG';
-import articleImg1 from '../assets/banner/IMG_9740.JPG';
-import articleImg2 from '../assets/banner/IMG_9749.JPG';
-import articleImg3 from '../assets/banner/IMG_9754.JPG';
+import banner from '../src/assets/banner/banner_1.JPG';
+import articleImg1 from '../src/assets/article/IMG_9740.JPG';
+import articleImg2 from '../src/assets/article/IMG_9749.JPG';
+import articleImg3 from '../src/assets/article/IMG_9754.JPG';
+import img1 from '../src/assets/img/IMG_9742.JPG';
+import img2 from '../src/assets/img/IMG_9743.JPG';
+import img3 from '../src/assets/img/IMG_9744.JPG';
+import img4 from '../src/assets/img/IMG_9747.JPG';
+import img5 from '../src/assets/img/IMG_9755.JPG';
 
 function App() {
   return (
     <div className="App">
       <div id="banner">
-        <img src="" />
+        <img src={banner} className="image-banner" />
       </div>
       <div className="article"></div>
       <div className="article"></div>
